86065f844c6dabd1eac57834fbd6a0e0cdadc5c5
hariharan <harihara95@gmail.com>
2026-08-15 14:41:15 +0530

    changes for render hosting

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,8 +15,8 @@ app.use("/api/buslocations", BusLocationsRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 startBusLocationService();
